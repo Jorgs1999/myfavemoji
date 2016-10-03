fcb2e858f44c1e3eaa3246287d5908e4bb3dfd7d
Jacob Jorgensen <17Jorgensenj@badinhs.org>
2016-10-03 08:25:49 -0400

    Data source and delegate

--- myfavemojis/ViewController.swift
+++ myfavemojis/ViewController.swift
@@ -8,12 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var cooltableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        cooltableview.dataSource = self //that data source makes where to look for the table
+        cooltableview.delegate = self //And contents
+        
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "😂"
+        return cell
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
